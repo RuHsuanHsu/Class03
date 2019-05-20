d2227ebdf43fe8c4e772444ef37c81cf48ef1c20
RuHsuanHsu <helen4525@gmail.com>
2019-05-20 12:02:13 +0800

    20190520 class

--- Class03/AppDelegate.swift
+++ Class03/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import WebKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var netWorker : AsyncRequestWorker?
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
     }
 
--- Class03/MyBrowserViewController.swift
+++ Class03/MyBrowserViewController.swift
@@ -9,8 +9,16 @@
 import UIKit
 import WebKit
 
-class MyBrowserViewController: UIViewController, UITextFieldDelegate{
-
+class MyBrowserViewController: UIViewController, UITextFieldDelegate, AsyncReponseDelegate {
+    
+    
+    func receviedReponse(_ sender: AsyncRequestWorker, responseString: String, tag: Int) {
+     print(responseString)
+        
+        myWebView.loadHTMLString(responseString, baseURL: URL(string: "https://www.google.com")!)
+    }
+    
+    
     
     @IBOutlet weak var btnGoBottomConstraint: NSLayoutConstraint!
     
@@ -18,9 +26,15 @@ class MyBrowserViewController: UIViewController, UITextFieldDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-        myWebView.load(URLRequest(url: URL(string: "https://www.google.com.tw")!))
+        
+        let workder : AsyncRequestWorker = AsyncRequestWorker()
+
+        workder.reponseDelegate = self
+        workder.getResponse(from: "https://www.google.com", tag: 1)
+        
+        //myWebView.load(URLRequest(url: URL(string: "https://www.google.com.tw")!))
     }
     
     override func viewWillAppear(_ animated: Bool) {
